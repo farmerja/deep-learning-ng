e447531fc6b92314547f014650a3acad73082543
farmerja <89936582+farmerja@users.noreply.github.com>
2021-09-15 10:47:31 -0400

    Add files via upload

---
+++ Recurrent Nerual Networks/transformers/Transformers in TensorFlow/transformer_tf.py
@@ -0,0 +1,439 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import tensorflow as tf
+import pandas as pd
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.layers import Embedding, MultiHeadAttention, Dense, Input, Dropout, LayerNormalization
+from transformers import DistilBertTokenizerFast #, TFDistilBertModel
+from transformers import TFDistilBertForTokenClassification
+from tqdm import tqdm_notebook as tqdm
+
+# %%
+
+def get_angles(pos, i, d):
+    """
+    Get the angles for the positional encoding
+    
+    Arguments:
+        pos -- Column vector containing the positions [[0], [1], ...,[N-1]]
+        i --   Row vector containing the dimension span [[0, 1, 2, ..., M-1]]
+        d(integer) -- Encoding size
+    
+    Returns:
+        angles -- (pos, d) numpy array 
+    """
+    D = 10000 ** (2 * (i // 2) / d)
+    angles = pos / D
+    
+    return angles
+
+# %%
+
+def get_angles_test(target):
+    position = 4
+    d_model = 16
+    pos_m = np.arange(position)[:, np.newaxis]
+    dims = np.arange(d_model)[np.newaxis, :]
+
+    result = target(pos_m, dims, d_model)
+
+    assert type(result) == np.ndarray, "You must return a numpy ndarray"
+    assert result.shape == (position, d_model), f"Wrong shape. We expected: ({position}, {d_model})"
+    assert np.sum(result[0, :]) == 0
+    assert np.isclose(np.sum(result[:, 0]), position * (position - 1) / 2)
+    even_cols =  result[:, 0::2]
+    odd_cols = result[:,  1::2]
+    assert np.all(even_cols == odd_cols), "Submatrices of odd and even columns must be equal"
+    limit = (position - 1) / np.power(10000,14.0/16.0)
+    assert np.isclose(result[position - 1, d_model -1], limit ), f"Last value must be {limit}"
+
+    print("\033[92mAll tests passed")
+
+get_angles_test(get_angles)
+
+# Example
+position = 4
+d_model = 8
+pos_m = np.arange(position)[:, np.newaxis]
+dims = np.arange(d_model)[np.newaxis, :]
+get_angles(pos_m, dims, d_model)
+
+# %%
+
+def positional_encoding(positions, d):
+    """
+    Precomputes a matrix with all the positional encodings 
+    
+    Arguments:
+        positions (int) -- Maximum number of positions to be encoded 
+        d (int) -- Encoding size 
+    
+    Returns:
+        pos_encoding -- (1, position, d_model) A matrix with the positional encodings
+    """
+    # initialize a matrix angle_rads of all the angles 
+    angle_rads = get_angles(np.arange(positions)[:, np.newaxis],
+                            np.arange(d)[np.newaxis, :],
+                            d)
+  
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+  
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    
+    pos_encoding = angle_rads[np.newaxis, ...]
+    
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+# %%
+
+def positional_encoding_test(target):
+    position = 8
+    d_model = 16
+
+    pos_encoding = target(position, d_model)
+    sin_part = pos_encoding[:, :, 0::2]
+    cos_part = pos_encoding[:, :, 1::2]
+
+    assert tf.is_tensor(pos_encoding), "Output is not a tensor"
+    assert pos_encoding.shape == (1, position, d_model), f"Wrong shape. We expected: (1, {position}, {d_model})"
+
+    ones = sin_part ** 2  +  cos_part ** 2
+    assert np.allclose(ones, np.ones((1, position, d_model // 2))), "Sum of square pairs must be 1 = sin(a)**2 + cos(a)**2"
+    
+    angs = np.arctan(sin_part / cos_part)
+    angs[angs < 0] += np.pi
+    angs[sin_part.numpy() < 0] += np.pi
+    angs = angs % (2 * np.pi)
+    
+    pos_m = np.arange(position)[:, np.newaxis]
+    dims = np.arange(d_model)[np.newaxis, :]
+
+    trueAngs = get_angles(pos_m, dims, d_model)[:, 0::2] % (2 * np.pi)
+    
+    assert np.allclose(angs[0], trueAngs), "Did you apply sin and cos to even and odd parts respectively?"
+ 
+    print("\033[92mAll tests passed")
+
+    
+positional_encoding_test(positional_encoding)
+
+# %%
+
+pos_encoding = positional_encoding(50, 512)
+
+print (pos_encoding.shape)
+
+plt.pcolormesh(pos_encoding[0], cmap='RdBu')
+plt.xlabel('d')
+plt.xlim((0, 512))
+plt.ylabel('Position')
+plt.colorbar()
+plt.show()
+
+# %%
+
+def create_padding_mask(seq):
+    """
+    Creates a matrix mask for the padding cells
+    
+    Arguments:
+        seq -- (n, m) matrix
+    
+    Returns:
+        mask -- (n, 1, 1, m) binary tensor
+    """
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+  
+    # add extra dimensions to add the padding
+    # to the attention logits.
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+x = tf.constant([[7., 6., 0., 0., 1.], [1., 2., 3., 0., 0.], [0., 0., 0., 4., 5.]])
+print(create_padding_mask(x))
+
+# %%
+
+print(tf.keras.activations.softmax(x))
+print(tf.keras.activations.softmax(x + create_padding_mask(x) * -1.0e9))
+
+# %%
+
+def create_look_ahead_mask(size):
+    """
+    Returns an upper triangular matrix filled with ones
+    
+    Arguments:
+        size -- matrix size
+    
+    Returns:
+        mask -- (size, size) tensor
+    """
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+x = tf.random.uniform((1, 3))
+temp = create_look_ahead_mask(x.shape[1])
+temp
+
+# %%
+
+def scaled_dot_product_attention(q, k, v, mask):
+    """
+    Calculate the attention weights.
+      q, k, v must have matching leading dimensions.
+      k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+      The mask has different shapes depending on its type(padding or look ahead) 
+      but it must be broadcastable for addition.
+
+    Arguments:
+        q -- query shape == (..., seq_len_q, depth)
+        k -- key shape == (..., seq_len_k, depth)
+        v -- value shape == (..., seq_len_v, depth_v)
+        mask: Float tensor with shape broadcastable 
+              to (..., seq_len_q, seq_len_k). Defaults to None.
+
+    Returns:
+        output -- attention_weights
+    """
+    
+    # Q*K'
+    matmul_qk = tf.matmul(q, k, transpose_b = True)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    # softmax is normalized on the last axis (seq_len_k) so that the scores
+    # add up to 1.
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis = -1)  # (..., seq_len_q, seq_len_k)
+    # attention_weights * V
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+    
+    return output, attention_weights
+
+# %%
+
+def scaled_dot_product_attention_test(target):
+    q = np.array([[1, 0, 1, 1], [0, 1, 1, 1], [1, 0, 0, 1]]).astype(np.float32)
+    k = np.array([[1, 1, 0, 1], [1, 0, 1, 1 ], [0, 1, 1, 0], [0, 0, 0, 1]]).astype(np.float32)
+    v = np.array([[0, 0], [1, 0], [1, 0], [1, 1]]).astype(np.float32)
+
+    attention, weights = target(q, k, v, None)
+    assert tf.is_tensor(weights), "Weights must be a tensor"
+    assert tuple(tf.shape(weights).numpy()) == (q.shape[0], k.shape[1]), f"Wrong shape. We expected ({q.shape[0]}, {k.shape[1]})"
+    assert np.allclose(weights, [[0.2589478,  0.42693272, 0.15705977, 0.15705977],
+                                   [0.2772748,  0.2772748,  0.2772748,  0.16817567],
+                                   [0.33620113, 0.33620113, 0.12368149, 0.2039163 ]])
+
+    assert tf.is_tensor(attention), "Output must be a tensor"
+    assert tuple(tf.shape(attention).numpy()) == (q.shape[0], v.shape[1]), f"Wrong shape. We expected ({q.shape[0]}, {v.shape[1]})"
+    assert np.allclose(attention, [[0.74105227, 0.15705977],
+                                   [0.7227253,  0.16817567],
+                                   [0.6637989,  0.2039163 ]])
+
+    mask = np.array([0, 0, 1, 0])
+    attention, weights = target(q, k, v, mask)
+
+    assert np.allclose(weights, [[0.30719590187072754, 0.5064803957939148, 0.0, 0.18632373213768005],
+                                 [0.3836517333984375, 0.3836517333984375, 0.0, 0.2326965481042862],
+                                 [0.3836517333984375, 0.3836517333984375, 0.0, 0.2326965481042862]]), "Wrong masked weights"
+    assert np.allclose(attention, [[0.6928040981292725, 0.18632373213768005],
+                                   [0.6163482666015625, 0.2326965481042862], 
+                                   [0.6163482666015625, 0.2326965481042862]]), "Wrong masked attention"
+    
+    print("\033[92mAll tests passed")
+    
+scaled_dot_product_attention_test(scaled_dot_product_attention)
+
+
+# %%
+
+def FullyConnected(embedding_dim, fully_connected_dim):
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(fully_connected_dim, activation='relu'),  # (batch_size, seq_len, dff)
+        tf.keras.layers.Dense(embedding_dim)  # (batch_size, seq_len, d_model)
+    ])
+
+# %%
+
+class EncoderLayer(tf.keras.layers.Layer):
+    """
+    The encoder layer is composed by a multi-head self-attention mechanism,
+    followed by a simple, positionwise fully connected feed-forward network. 
+    This archirecture includes a residual connection around each of the two 
+    sub-layers, followed by layer normalization.
+    """
+    def __init__(self, embedding_dim, num_heads, fully_connected_dim, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(EncoderLayer, self).__init__()
+
+        self.mha = MultiHeadAttention(num_heads=num_heads,
+                                      key_dim=embedding_dim)
+
+        self.ffn = FullyConnected(embedding_dim=embedding_dim,
+                                  fully_connected_dim=fully_connected_dim)
+
+        self.layernorm1 = LayerNormalization(epsilon=layernorm_eps)
+        self.layernorm2 = LayerNormalization(epsilon=layernorm_eps)
+
+        self.dropout1 = Dropout(dropout_rate)
+        self.dropout2 = Dropout(dropout_rate)
+    
+    def call(self, x, training, mask):
+        """
+        Forward pass for the Encoder Layer
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, input_seq_len, fully_connected_dim)
+            training -- Boolean, set to true to activate
+                        the training mode for dropout layers
+            mask -- Boolean mask to ensure that the padding is not 
+                    treated as part of the input
+        Returns:
+            out2 -- Tensor of shape (batch_size, input_seq_len, fully_connected_dim)
+        """
+        # calculate self-attention using mha(~1 line)
+        attn_output = self.mha(x, x, x, mask)  # Self attention (batch_size, input_seq_len, fully_connected_dim)
+        
+        # apply dropout layer to the self-attention output (~1 line)
+        attn_output = self.dropout1(attn_output, training = training)
+        
+        # apply layer normalization on sum of the input and the attention output to get the  
+        # output of the multi-head attention layer (~1 line)
+        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, fully_connected_dim)
+
+        # pass the output of the multi-head attention layer through a ffn (~1 line)
+        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, fully_connected_dim)
+        
+        # apply dropout layer to ffn output (~1 line)
+        ffn_output = self.dropout2(ffn_output, training = training)
+        
+        # apply layer normalization on sum of the output from multi-head attention and ffn output to get the
+        # output of the encoder layer (~1 line)
+        out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, fully_connected_dim)
+        
+        return out2
+
+# %%
+
+def EncoderLayer_test(target):
+    q = np.array([[[1, 0, 1, 1], [0, 1, 1, 1], [1, 0, 0, 1]]]).astype(np.float32)
+    encoder_layer1 = EncoderLayer(4, 2, 8)
+    tf.random.set_seed(10)
+    encoded = encoder_layer1(q, True, np.array([[1, 0, 1]]))
+    
+    assert tf.is_tensor(encoded), "Wrong type. Output must be a tensor"
+    assert tuple(tf.shape(encoded).numpy()) == (1, q.shape[1], q.shape[2]), f"Wrong shape. We expected ((1, {q.shape[1]}, {q.shape[2]}))"
+
+    assert np.allclose(encoded.numpy(), 
+                       [[-0.5214877 , -1.001476  , -0.12321664,  1.6461804 ],
+                       [-1.3114998 ,  1.2167752 , -0.5830886 ,  0.6778133 ],
+                       [ 0.25485858,  0.3776546 , -1.6564771 ,  1.023964  ]],), "Wrong values"
+    
+    print("\033[92mAll tests passed")
+    
+
+EncoderLayer_test(EncoderLayer)
+
+# %%
+
+class Encoder(tf.keras.layers.Layer):
+    """
+    The entire Encoder starts by passing the input to an embedding layer 
+    and using positional encoding to then pass the output through a stack of
+    encoder Layers
+        
+    """   
+    def __init__(self, num_layers, embedding_dim, num_heads, fully_connected_dim, input_vocab_size,
+               maximum_position_encoding, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(Encoder, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.embedding = Embedding(input_vocab_size, self.embedding_dim)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, 
+                                                self.embedding_dim)
+
+
+        self.enc_layers = [EncoderLayer(embedding_dim=self.embedding_dim,
+                                        num_heads=num_heads,
+                                        fully_connected_dim=fully_connected_dim,
+                                        dropout_rate=dropout_rate,
+                                        layernorm_eps=layernorm_eps) 
+                           for _ in range(self.num_layers)]
+
+        self.dropout = Dropout(dropout_rate)
+        
+    def call(self, x, training, mask):
+        """
+        Forward pass for the Encoder
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, input_seq_len)
+            training -- Boolean, set to true to activate
+                        the training mode for dropout layers
+            mask -- Boolean mask to ensure that the padding is not 
+                    treated as part of the input
+        Returns:
+            out2 -- Tensor of shape (batch_size, input_seq_len, fully_connected_dim)
+        """
+
+        seq_len = tf.shape(x)[1]
+        
+        # Pass input through the Embedding layer
+        x = self.embedding(x)  # (batch_size, input_seq_len, fully_connected_dim)
+        # Scale embedding by multiplying it by the square root of the embedding dimension
+        x *= tf.math.sqrt(tf.cast(self.embedding_dim, tf.float32))
+        # Add the position encoding to embedding
+        x += self.pos_encoding[:, :seq_len, :]
+        # Pass the encoded embedding through a dropout layer
+        x = self.dropout(x, training = training)
+        # Pass the output through the stack of encoding layers 
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, training, mask)
+
+        return x  # (batch_size, input_seq_len, fully_connected_dim)
+
+# %%
+
+def Encoder_test(target):
+    tf.random.set_seed(10)
+    
+    embedding_dim=4
+    
+    encoderq = target(num_layers=2,
+                      embedding_dim=embedding_dim,
+                      num_heads=2,
+                      fully_connected_dim=8,
+                      input_vocab_size=32,
+                      maximum_position_encoding=5)
+    
+    x = np.array([[2, 1, 3], [1, 2, 0]])
+    
+    encoderq_output = encoderq(x, True, None)
+    
+    assert tf.is_tensor(encoderq_output), "Wrong type. Output must be a tensor"
+    assert tuple(tf.shape(encoderq_output).numpy()) == (x.shape[0], x.shape[1], embedding_dim), f"Wrong shape. We expected ({eshape[0]}, {eshape[1]}, {embedding_dim})"
+    assert np.allclose(encoderq_output.numpy(), 
+                       [[[-0.40172306,  0.11519244, -1.2322885,   1.5188192 ],
+                         [ 0.4017268,   0.33922842, -1.6836855,   0.9427304 ],
+                         [ 0.4685002,  -1.6252842,   0.09368491,  1.063099  ]],
+                        [[-0.3489219,   0.31335592, -1.3568854,   1.3924513 ],
+                         [-0.08761203, -0.1680029,  -1.2742313,   1.5298463 ],
+                         [ 0.2627198,  -1.6140151,   0.2212624 ,  1.130033  ]]]), "Wrong values"
+    
+    print("\033[92mAll tests passed")
+    
+Encoder_test(Encoder)
+
